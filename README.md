d293414ff95eb8990605426a5e5a6d0a04f1d1f9
maxykoin <stayerim@gmail.com>
2022-08-27 19:01:29 -0300

    implementation of the sympy library

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Spatial Geometry
-With this program you'll be able to calculate the area and volume of 5 different solids: pyramids cones, cylinders, spheres, and polygons.
+With this program you'll be able to calculate the area and volume of 5 different solids: pyramids, cones, cylinders, spheres, and polygons. It supports undisclosed square root values (like √3) and undisclosed π value for more specific calculus with the use of the [sympy library](https://docs.sympy.org). If any bugs or erros happen please report it me!
 
-It's an unser input through the cmd based program, if any bugs or erros happen please report it me! This was a passion project to test my python knowledge and an attempt to memorize the formulas for my math exam, I tried to make it as complex as possible based on my knowledge of python (and math) to this point in my apprenticeship (at the time of uploading my first commit) of just one week. 
+This is a passion project, made to test my python knowledge and double feautures as an attempt to memorize the formulas for a math exam.
+
+## How to use 
+- Download the *geometry.py* file
+- Use pip to install the Sympy library
+``` bash
+pip install sympy
+```
+- Have fun!
 
 ## What I Learned
 - Better understading of while loops
@@ -9,4 +17,5 @@ It's an unser input through the cmd based program, if any bugs or erros happen p
 - Better nested Ifs
 - Better variable naming 
 - Memorized some formulas for my exam :]
+- Learnt more about de Sympy library
 
--- geometry.py
+++ geometry.py
@@ -1,118 +1,172 @@
+import sympy
 forma = -1
 
 while forma != 0:
     print('~'*30)
-    forma = int(input('''
-    [ 0 ] TO EXIT
-    [ 1 ] PIRÂMIDE
-    [ 2 ] CONE
-    [ 3 ] CILINDRO
-    [ 4 ] PRISMA
-    [ 5 ] ESFERA
-    Qual é sua escolha? 
-    '''))
+    forma = int(input('''Choose which solid do you want to calculate:
+        [ 1 ] PYRAMID
+        [ 2 ] CONE
+        [ 3 ] CYLINDER
+        [ 4 ] POLYGON
+        [ 5 ] SPHERE
+        [ 0 ] TO EXIT
+    Enter here: '''))
     print('~'*30)
-
-    ### PYRAMID ###
+    
+#### PYRAMIDS ###############################################################################################
     while forma == 1:
-        formula = input('Digite A para área e V para volume [0 pra voltar]: ').lower()
+        formula = input('Write A for area or V for volume [0 to exit]: ').lower()
         if formula == 'a':
-            print('~'*10, 'Área Lateral', '~'*10)
-            pb = float(input('Digite o valor do lado base: '))
-            ph = float(input('Digite o valor da altura: '))
+            print('~'*10, '''Triangle's Area''', '~'*10)
+            b = float(input('Digite o valor do lado base: '))
+            h = float(input('Digite o valor da altura: '))
             print('~'*30)
-            al = pb * ph / 2
 
-            print('~'*30)
-            base = int(input('''
-            Escolha a base:
-            [ 1 ] QUADRADO
-            [ 2 ] RETÂNGULO
-            [ 3 ] HEXAGONO
-            '''))
+            base = int(input('''Choose the base:
+                [ 1 ] SQUARE
+                [ 2 ] RETANGLE
+                [ 3 ] HEXAGON
+            Enter here: '''))
             print('~'*30)
 
             while base == 1:
-                ab = float(input('Digite o valor da aresta: '))
-                ab = ab ** 2
-                al * 4
+                l = float(input('Digite o valor da aresta: '))
+                ab = l**2
+                al = b * h / 2 * 4
                 break
 
             while base == 2:
                 b = float(input('Digite o valor da base: '))
                 h = float(input('Digite o valor da altura: '))
-                ab = b * h
-                al * 4
-                break
-
-            while base == 3:
-                b = float(input('Digite o valor da base: '))
-                l = float(input('Digite o valor do lado: '))
-                ra = float(input('Digite o valor da raiz de 3: '))
-                ab = b * (l)**2 * ra / 4
-                al * 6
+                ab = b * h 
+                al = b * h / 2 * 4
                 break
             
-            print(''' Esse sólido tem a area da base de valor {} e a area lateral de valor {}.
+            while base == 3:
+                definition = input('Is the value of √3 defined (Y/N)? ').lower()
+                if definition == 'y':
+                    b = float(input('Digite o valor da base: '))
+                    l = float(input('Digite o valor do lado: '))
+                    ra = float(input('Digite o valor da raiz de 3: '))
+                    ab = b * (l)**2 * ra / 4
+                    al = b * h / 2 * 6
+                    break
+                
+                if definition == 'n':
+                    b = float(input('Digite o valor da base: '))
+                    l = float(input('Digite o valor do lado: '))
+                    ab = b * (l)**2 * sympy.sqrt(3) / 4
+                    al = b * h / 2 * 6
+                    break
+                
+            print('''Esse sólido tem a area da base de valor {} e a area lateral de valor {}.
             O valor da área total é de {}cm'''.format(ab, al, ab + al))
 
         if formula == 'v':
-            ab = float(input('Digite a area da base: '))
-            h = float(input('Digite a altura: '))
-            print('O volume desse sólido é {}cm'.format(ab * h / 3))
-            print('~'*30)
-            break
+            definition = input('Is there any undefined square roots (Y/N)? ').lower()
+            if definition == 'y':
+                sqrt = int(input('Write down the number (without the square root): '))
+                ab = float(input('Digite a area da base (w/o sqrt): '))
+                h = float(input('Digite a altura (w/o sqrt): '))
+                print('O volume desse sólido é {} {} cm'.format(ab * h / 3, sympy.sqrt(sqrt)))
+                print('~'*30)
+                break
+    
+            if definition == 'n':
+                ab = float(input('Digite a area da base: '))
+                h = float(input('Digite a altura: '))
+                print('O volume desse sólido é {}cm'.format(ab * h / 3))
+                print('~'*30)
+                break
+            
         if formula == '0':
             break
-        
-    ### CONES ###
+#### CONES #####################################################################################################    
     while forma == 2:
-        formula = input('Digite A para área e V para volume [0 pra voltar]: ').lower()
+        formula = input('Write A for area or V for volume [0 to exit]: ').lower()
         if formula == 'a':
-            pi = float(input('Digite o valor de pi: '))
-            r = float(input('Digite o valor do raio: '))
-            g = float(input('Digite o valor da geratriz: '))
-            print('''Esse sólido tem a area da base de valor {} e a area lateral de valor {}
-            A area total do sólido é de {}'''.format(pi * (r)**2 + pi * r * g ))
-
+            definition = input('Is the value of π defined (Y/N)? ').lower()
+            if definition == 'n':
+                r = float(input('Digite o valor do raio: '))
+                g = float(input('Digite o valor da geratriz: '))
+                
+                print('''Esse sólido tem a area da base de valor {} e a area lateral de valor {}
+                A area total do sólido é de {}'''.format(sympy.pi * (r)**2, sympy.pi * r * g, sympy.pi * (r)**2 + sympy.pi * r * g))
+                break 
+            
+            if definition == 'y':
+                pi = float(input('Digite o valor de pi: '))
+                r = float(input('Digite o valor do raio: '))
+                g = float(input('Digite o valor da geratriz: '))
+                print('''Esse sólido tem a area da base de valor {} e a area lateral de valor {}
+                A area total do sólido é de {}'''.format(pi * (r)**2, pi * r * g, pi * (r)**2 + pi * r * g))
+                break 
+            
         if formula == 'v':
-            ab = float(input('Digite o valor da area da base: '))
-            h = float(input('Digite o valor da altura: '))
-            print('O volume desse sólido é de {}'.format(ab * h / 3))
-            break
-        
+            definition = input('Is the value of pi undefined (Y/N)? ').lower()
+            if definition == 'n':
+                ab = float(input('Digite a area da base: '))
+                h = float(input('Digite a altura: '))
+                print('O volume desse sólido é de {} {}'.format(ab * h / 3, sympy.pi))
+                break
+            
+            if definition == 'y':
+                ab = float(input('Digite o valor da area da base: '))
+                h = float(input('Digite o valor da altura: '))
+                print('O volume desse sólido é de {}'.format(ab * h / 3))
+                break
+            
         if formula == '0':
-            break
-        
-    ### CYLINDERS ###
+            break 
+#### CYLINDERS ###################################################################################################
     while forma == 3:
-        formula = input('Digite A para área e V para volume [0 pra voltar]: ').lower()
+        formula = input('Write A for area or V for volume [0 to exit]: ').lower()
         if formula == 'a':
-            pi = float(input('Digite o valor de pi: '))
-            r = float(input('Digite o valor do raio: '))
-            h = float(input('Digite o valor da altura: '))
-            ab = 2 * pi * (r)**2
-            al = 2 * pi * r
+            definition = input('Is the value of π defined (Y/N)? ').lower()
+            if definition == 'y':
+                pi = float(input('Digite o valor de pi: '))
+                r = float(input('Digite o valor do raio: '))
+                h = float(input('Digite o valor da altura: '))
+                ab = 2 * pi * (r)**2
+                al = 2 * pi * r
 
-            print('''Esse sólido tem a area da base de valor {} e a area lateral de valor {}
-            A area total do sólido é de {}'''.format(ab, al, ab + al))
+                print('''Esse sólido tem a area da base de valor {} e a area lateral de valor {}
+                A area total do sólido é de {}'''.format(ab, al, ab + al))
+                break 
+            
+            if definition == 'n':
+                r = float(input('Digite o valor do raio: '))
+                h = float(input('Digite o valor da altura: '))
+                ab = 2 * sympy.pi * (r)**2
+                al = 2 * sympy.pi * r
 
+                print('''Esse sólido tem a area da base de valor {} e a area lateral de valor {}
+                A area total do sólido é de {}'''.format(ab, al, ab + al))
+                break
+            
         if formula == 'v':
-            ab = float(input('Digite o valor da area da base: '))
-            h = float(input('Digite o valor da altura: '))
-            print('O volume desse sólido é de {}'.format(ab * h))
-            break
-    
+            definition = input('Is the value of pi undefined (Y/N)? ').lower()
+            if definition == 'n':
+                ab = float(input('Digite a area da base: '))
+                h = float(input('Digite a altura: '))
+                print('O volume desse sólido é de {} {}'.format(ab * h, sympy.pi))
+                break
+            
+            if definition == 'y':
+                ab = float(input('Digite o valor da area da base: '))
+                h = float(input('Digite o valor da altura: '))
+                print('O volume desse sólido é de {}'.format(ab * h))
+                break
+            
         if formula == '0':
-            break
-    
-    ### POLYGONS ###
+            break 
+#### POLYGONS ###################################################################################################
     while forma == 4:
-        formula = input('Digite A para área e V para volume [0 pra voltar]: ').lower()
+        formula = input('Write A for area or V for volume [0 to exit]: ').lower()
         if formula == 'a':
             print('~'*30)
             base = int(input('''
-            Escolha a base (calcule de uma base por vez):
+            Escolha a base (para bases diferentes, calcule uma por vez):
             [ 1 ] QUADRADO
             [ 2 ] RETÂNGULO
             [ 3 ] HEXAGONO
@@ -131,15 +185,22 @@ while forma != 0:
                 break
 
             while base == 3:
-                b = float(input('Digite o valor da base: '))
-                l = float(input('Digite o valor do lado: '))
-                ra = float(input('Digite o valor da raiz de 3 (3 ou o valor apresentado na questão): '))
-                ab = ab * l** 2 * ra / 4
-                break
+                definition = input('Is the value of √3 defined (Y/N)? ').lower()
+                if definition == 'y':
+                    b = float(input('Digite o valor da base: '))
+                    l = float(input('Digite o valor do lado: '))
+                    ra = float(input('Digite o valor da raiz de 3: '))
+                    ab = b * (l)**2 * ra / 4
+                    break
+                if definition == 'n':
+                    b = float(input('Digite o valor da base: '))
+                    l = float(input('Digite o valor do lado: '))
+                    ab = b * (l)**2 * sympy.sqrt(3) / 4
+                    break
         
             print('~'*30)
             lateral = int(input('''
-            Escolha a area lateral (calcule de uma lateral por vez):
+            Escolha a area lateral (para laterais diferentes, calcule uma por vez):
             [ 1 ] QUADRADO
             [ 2 ] RETÂNGULO
             [ 3 ] HEXAGONO
@@ -159,11 +220,19 @@ while forma != 0:
                 break
 
             while lateral == 3:
-                b = float(input('Digite o valor da base: '))
-                l = float(input('Digite o valor do lado: '))
-                ra = float(input('Digite o valor da raiz de 3 (3 ou o valor apresentado na questão): '))
-                al = al * l** 2 * ra / 4
-                break
+                definition = input('Is the value of √3 defined (Y/N)? ').lower()
+                if definition == 'y':
+                    b = float(input('Digite o valor da base: '))
+                    l = float(input('Digite o valor do lado: '))
+                    ra = float(input('Digite o valor da raiz de 3: '))
+                    ab = b * (l)**2 * ra / 4
+                    break
+                
+                if definition == 'n':
+                    b = float(input('Digite o valor da base: '))
+                    l = float(input('Digite o valor do lado: '))
+                    ab = b * (l)**2 * sympy.sqrt(3) / 4
+                    break
 
             while lateral == 4:
                 b = float(input('Digite o valor da base: '))
@@ -176,32 +245,56 @@ while forma != 0:
             break
 
         if formula == 'v': 
-            ab = float(input('Digite a area da base: '))
-            h = float(input('Digite o valor da altura do solido: '))
-            print('O volume desse sólido com a area de base {} e altura de {}, é de {}'.format(ab, h, ab * h))
-            break
-
+            definition = input('Is there any undefined square roots (Y/N)? ').lower()
+            if definition == 'y':
+                sqrt = int(input('Write down the number (without the square root): '))
+                ab = float(input('Digite a area da base (w/o sqrt): '))
+                h = float(input('Digite a altura (w/o sqrt): '))
+                print('O volume desse sólido é {} {} cm'.format(ab * h, sympy.sqrt(sqrt)))
+                print('~'*30)
+                break
+    
+            if definition == 'n':
+                ab = float(input('Digite a area da base: '))
+                h = float(input('Digite a altura: '))
+                print('O volume desse sólido é {}cm'.format(ab * h))
+                print('~'*30)
+                break
+            
         if formula == '0':
             break
-        
-    ### SPHERE ###
+#### SPHERES ###################################################################################################
     while forma == 5: 
-        formula = input('Digite A para área e V para volume [0 pra voltar]: ').lower()
-        print('~'*30)
-
-        if formula == 'a':
-            pi = float(input('Digite o valor de pi: '))
-            r = float(input('Digite o valor do raio: '))
-            print('A area total da esfera é de {}'.format(4 * pi * (r)**2))
+        formula = input('Write A for area or V for volume [0 to exit]: ').lower()
         
+        if formula == 'a':
+            definition = input('Is the value of π defined (Y/N)? ').lower()
+            if definition == 'y':
+                pi = float(input('Digite o valor de pi: '))
+                r = float(input('Digite o valor do raio: '))
+                print('A area total da esfera é de {}'.format(4 * pi * (r)**2)) 
+                break
+            
+            if definition == 'n':
+                r = float(input('Digite o valor do raio: '))
+                print('A area total da esfera é de {}'.format(4 * sympy.pi * (r)**2))
+                break
+            
         print('~'*30)
 
         if formula == 'v':
-            pi = float(input('Digite o valor de pi: '))
-            r = float(input('Digite o valor do raio: '))
-            print('O volume total da esfera é de {}'.format(4 * pi * (r)**3 / 3))
-            break
-        
+            definition = input('Is the value of π defined (Y/N)? ').lower()
+            if definition == 'y':
+                pi = float(input('Digite o valor de pi: '))
+                r = float(input('Digite o valor do raio: '))
+                print('A area total da esfera é de {}'.format(4 * pi * (r)**3 / 3))
+                break
+            
+            if definition == 'n':
+                r = float(input('Digite o valor do raio: '))
+                print('A area total da esfera é de {}'.format(4 * sympy.pi * (r)**3 / 3))
+                break
+            
         if formula == '0':
             break
 
